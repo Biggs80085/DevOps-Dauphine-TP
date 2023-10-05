be0e2b7287e0408b4bd86dc0f876ca079d0ef62e
saoulajane <hamzasaoulajane9@gmail.com>
2023-10-05 09:20:48 +0000

    push2

--- main.tf
+++ main.tf
@@ -27,6 +27,10 @@ resource "google_project_service" "api-cloudbuild" {
     service ="cloudbuild.googleapis.com"
 }
 
+resource "google_project_service" "api-cloudrun" {
+    service ="run.googleapis.com"
+}
+
 resource "google_artifact_registry_repository" "artifact" {
     repository_id = "website-tools"
     location = "us-west2"
@@ -38,4 +42,45 @@ resource "google_sql_user" "wordpress" {
    name     = "wordpress"
    instance = "main-instance"
    password = "ilovedevops"
+}
+
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-west2"
+
+template {
+   spec {
+      containers {
+      image = "us-west2-docker.pkg.dev/devops-399708/website-tools/wordpress-image"
+      }
+   }
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "devops-399708:us-west2:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
